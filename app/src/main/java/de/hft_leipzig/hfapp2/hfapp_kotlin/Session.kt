b4d3b85edfdd2b000c176a9a96a52c81360ea39b
Igor Kim @ HfTL <kim@hft-leipzig.de>
2019-05-17 16:13:29 +0200

    Make sessions as Parcelable Add exportedStatus field Addresses #8

--- app/src/main/java/de/hft_leipzig/hfapp2/hfapp_kotlin/Session.kt
+++ app/src/main/java/de/hft_leipzig/hfapp2/hfapp_kotlin/Session.kt
@@ -1,11 +1,30 @@
 package de.hft_leipzig.hfapp2.hfapp_kotlin
 
+import android.os.Parcelable
+import android.os.Parcel
 
-data class Session(val uid: Int) {
+
+
+
+data class Session(val uid: Int): Parcelable {
     var sessionID: String? = ""
+    var exportedStatus: Int? = 0
 
     constructor(_sessionID: String) : this(0) {
         sessionID = _sessionID
+    }
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!)
+
+    companion object CREATOR : Parcelable.Creator<Session> {
+        override fun createFromParcel(parcel: Parcel): Session {
+            return Session(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Session?> {
+            return arrayOfNulls(size)
+        }
     }
 
     override fun equals(other: Any?): Boolean {
@@ -14,5 +33,14 @@ data class Session(val uid: Int) {
 
     override fun toString(): String {
         return sessionID!!
+    }
+
+
+    override fun writeToParcel(parcel: Parcel, i: Int) {
+        parcel.writeString(sessionID)
+    }
+
+    override fun describeContents(): Int {
+        return 0
     }
 }
