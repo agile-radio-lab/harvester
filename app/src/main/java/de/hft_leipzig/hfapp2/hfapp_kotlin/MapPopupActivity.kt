7e4b1a7d027850ee85c106ed395469361b5df938
Igor Kim @ HfTL <kim@hft-leipzig.de>
2019-05-17 16:10:07 +0200

    Save selected sessions when returning from the map Addresses #7

--- app/src/main/java/de/hft_leipzig/hfapp2/hfapp_kotlin/MapPopupActivity.kt
+++ app/src/main/java/de/hft_leipzig/hfapp2/hfapp_kotlin/MapPopupActivity.kt
@@ -1,7 +1,9 @@
 package de.hft_leipzig.hfapp2.hfapp_kotlin
 
+import android.app.Activity
 import android.arch.persistence.room.Room
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -16,6 +18,11 @@ import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
 import com.mapbox.mapboxsdk.style.layers.LineLayer
 import com.mapbox.mapboxsdk.style.layers.Property
 import android.graphics.Color
+import android.support.v7.app.AlertDialog
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.mapbox.mapboxsdk.camera.CameraPosition
 import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory
@@ -26,14 +33,17 @@ class MapPopupActivity : AppCompatActivity() {
     private lateinit var db: AppDatabase
     private var sessionID: String = ""
     private var mapView: MapView? = null
+    private var exportedSessions: ArrayList<Session> = ArrayList()
     private lateinit var routeCoordinates: ArrayList<Point>
 
     companion object {
         const val SESSION_ID = "sessionID"
+        const val EXPORTED_SESSIONS = "exportedSessions"
 
-        fun newIntent(context: Context, session: Session): Intent {
+        fun newIntent(context: Context, session: Session, exportedSessions: ArrayList<Session>): Intent {
             val detailIntent = Intent(context, MapPopupActivity::class.java)
             detailIntent.putExtra(SESSION_ID, session.sessionID)
+            detailIntent.putExtra(EXPORTED_SESSIONS, exportedSessions)
             return detailIntent
         }
     }
@@ -42,6 +52,12 @@ class MapPopupActivity : AppCompatActivity() {
         override fun run() {
             val listSessionList = db.measurementPointDao().getMapPointsBySessionID(sessionID)
             mapPoints = ArrayList(listSessionList)
+        }
+    }
+
+    internal inner class DeleteSessionFromDB : Runnable {
+        override fun run() {
+            db.measurementPointDao().deleteBySessionID(sessionID)
         }
     }
 
@@ -55,7 +71,7 @@ class MapPopupActivity : AppCompatActivity() {
         // Create a list to store our line coordinates.
         routeCoordinates = ArrayList()
         for (m in mapPoints) {
-            routeCoordinates.add(Point.fromLngLat(m.locLongitude, m.locLatidute))
+            routeCoordinates.add(Point.fromLngLat(m.locLongitude, m.locLatitude))
         }
     }
 
@@ -65,6 +81,7 @@ class MapPopupActivity : AppCompatActivity() {
         setContentView(R.layout.activity_map_popup)
 
         sessionID = intent.extras.getString(SESSION_ID)
+        exportedSessions = intent.extras.getParcelableArrayList<Session>(EXPORTED_SESSIONS)
         db = Room.databaseBuilder(
             applicationContext,
             AppDatabase::class.java, "measurements"
@@ -102,6 +119,12 @@ class MapPopupActivity : AppCompatActivity() {
         }
     }
 
+    override fun onBackPressed() {
+        val intent = Intent(this, SecondActivity::class.java)
+        intent.putParcelableArrayListExtra(EXPORTED_SESSIONS, exportedSessions)
+        setResult(Activity.RESULT_OK, intent)
+        super.onBackPressed()
+    }
 
     public override fun onResume() {
         super.onResume()
