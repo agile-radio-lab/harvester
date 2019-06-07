89f7a658838cb93fca3787b89138e65b3c035e46
Igor Kim @ HfTL <kim@hft-leipzig.de>
2019-06-07 12:51:23 +0200

    move permission control to a helper module

--- app/src/main/java/de/hft_leipzig/hfapp2/hfapp_kotlin/permissions_helper.kt
+++ app/src/main/java/de/hft_leipzig/hfapp2/hfapp_kotlin/permissions_helper.kt
@@ -1,2 +1,35 @@
 package de.hft_leipzig.hfapp2.hfapp_kotlin
 
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+
+const val PERMISSIONS_REQUEST_ALL = 0x1
+const val PERMISSIONS_REQUEST_WRITE_EXTERNAL = 0x2
+
+val ALL_PERMISSIONS = arrayOf(
+    android.Manifest.permission.ACCESS_COARSE_LOCATION,
+    android.Manifest.permission.ACCESS_FINE_LOCATION,
+    android.Manifest.permission.READ_PHONE_STATE
+)
+
+
+fun hasPermissions(context: Context?, permissions: Array<String>): Boolean {
+    if (context != null) {
+        for (permission in permissions) {
+            if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false
+            }
+        }
+    }
+    return true
+}
+
+fun getPermissions(context: Context?, permissions: Array<String>, requestID: Int): Boolean {
+    if (!hasPermissions(context, permissions)) {
+        ActivityCompat.requestPermissions(context as Activity, permissions, requestID)
+        return false
+    }
+    return true
+}
